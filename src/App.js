3e4c5fcd243e7995bae5dc10770f7956f496fdd6
Matthew De Rose <matthew.david.derose@gmail.com>
2019-03-16 16:01:54 -0700

    Created search function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import List from './components/List';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+  	super(props);
+  	const data = require('./data.json');
+  	this.state = {
+  		data: data
+  	}
+ 
+
+  }
   render() {
     return (
       <div className="App">
-        
+        {
+        	
+        }
         <header className="App-header">Lost and Found</header>
-        <List />
+        <h1>Dogs are listed with Name, Breed, Colour and Apparent Sex</h1>
+        <List data = {this.state.data} />
       </div>
     );
   }
--- src/components/List.js
+++ src/components/List.js
@@ -1,20 +1,69 @@
 import React, { Component } from 'react';
+import { debounce } from 'lodash';
 
 class List extends Component {
     constructor(props) {
         super(props);
-        const data = require('../data.json')
-        this.items = data.map((data) => <li>{data.Name} || {data.Breed} || {data.Sex} || {data.Color} </li>)
+        this.state = {
+            filtered: [],
+            search: '',
+            
+        }
+        this.handleChange = this.handleChange.bind(this);
+    
     }
+
+    debouncedHandleChange = debounce(this.handleChange, 200);
+
     render() {
+        const { search } = this.state;
+        console.log('l',search.length);
         return (
             <div>
+            <input type="text" className ="input" onChange={this.handleChange} placeholder="Search..."/>
                 <ul>
-                    {this.items}
+                    { search.length > 9 && this.state.filtered.map(dog => (<li key={dog}>{dog.Name}||{dog.Breed}||{dog.Color}||{dog.Sex}</li>))}
                 </ul>
             </div>
         );
     }
-}
+
+            componentDidMount() {
+            this.setState({
+                filtered: this.props.data
+            });
+        }
+
+        componentWillRecieveProps(nextProps) {
+            this.setState({
+                filtered: nextProps.data
+            });
+        }
+
+        handleChange(e) {
+            console.log('handleChange');
+            let currentList = [];
+            let newList = [];
+
+            if(e.target.value !== "") {
+                currentList = this.props.data;
+                newList = currentList.filter(datum => {
+                    const lc = datum.DateCreated.toLowerCase();
+                    const filter = e.target.value.toLowerCase();
+
+                    return lc.includes(filter);
+                });
+                console.log(newList);
+
+            } else {
+                newList =this.props.data;
+            }
+            this.setState({
+                filtered: newList,
+                search: e.target.value,
+            });
+
+            }
+        }
 
 export default List
