6bf92a27328c76c0c78bd10c9d674fbf9b78b92c
costewart <45110718+costewart@users.noreply.github.com>
2019-03-16 15:28:03 -0700

    Charlotte - css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import List from './components/List';
+import Search from './components/Search';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +8,36 @@ class App extends Component {
     return (
       <div className="App">
         
-        <header className="App-header">Lost and Found</header>
+        <div className="wrapper">
+        
+        <div className="main">
+
+        <div className="container">
+        
+        <div className="row">
+        
+        <div className="col-xs-3 title-container">
+        <header className="App-header">Lost and Found 
+        
+        <header className="App-header2"> <Search /></header>
+        
+        </header>
+        
+        </div>
+      
+        <div className="col-xs-7 form-container">
         <List />
+        </div>
+      
+      </div>
+      </div>
+      </div>
+      </div>
       </div>
     );
   }
-}
+};
+
+
 
 export default App;
