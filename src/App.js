d13a58f2a15d82ef811e025dfc7e76561e09b729
helenwxy <40188330+helenwxy@users.noreply.github.com>
2019-03-16 15:26:42 -0700

    embedded logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import List from './components/List';
 import './App.css';
+import vancityLogo from "../src/vancityLogo.png";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <img src={vancityLogo} alt="logo" width={200} height={100}/>
         <header className="App-header">Lost and Found</header>
         <List />
       </div>
